d9266925b4dd334628668068678e8f60762d72f9
Kenneth Chan <kc135576@aol.com>
2014-09-08 00:00:58 -0700

    Revert "dnacub3d.tk push"
    This reverts commit 54bb12e4c0e24459e7cbbb7d10ee0efadd2476c2.

--- index.php
+++ index.php
@@ -1,49 +1,9 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html>
 <head>
-	<title>cub01d&apos;s website</title>
-	<link rel="icon" type="image/jpeg" href="/images/icon.jpg" />
-	<style type="text/css">
-	body {font-family: fixedsys, "dejavu sans mono", courier, monospace; color:#d4c3b1; background-color:#000000; color:#ffffff;}
-	span {font-size: 90px;}
-	td {padding:15px; border: 1px solid white;}
-	a:link, a:visited {
-		text-decoration:none;
-		color:#0087E2;
-		font-weight:bold;
-		text-shadow:1px 1px 5px #999
-	}
 
-	a:hover {
-		text-decoration:underline;
-		color:#048;
-		text-shadow:1px 1px 5px #999
-	}
-        h1 {
-                color:white;
-        }
-	</style>
 </head>
 <body>
-	<div align="center">
-		<span>cub01d.github.io</span>
-		<br>
-                <h1>Welcome to cub01d's website.</h1>
-		<a href="/"><h4>click for recursive fun.</h4></a>
-		<br>
-		
-		<table>
-			<tr>
-				<td><a href="/">HOME</a></td>
-				<td><a href="/projects/">Projects</a></td>
-				<td><a href="/stuff/">Cool stuff</a></td>
-				<td><a href="/school">School stuff</a></td>
-                <td><a href="/about.html">About me</a></td>
-			</tr>
-		</table>
-		
-		<h1>Live to hack, live to learn.</h1>
-	</div>
+hi
 </body>
 </html>
-<!--
